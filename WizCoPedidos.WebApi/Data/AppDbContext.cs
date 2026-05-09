862d62311fbd90f097bf56cf6d10442abdceeafb
ViniciusSoares1943 <vinicius.soares.01@hotmail.com>
2026-05-09 14:39:35 -0300

    Adicionando AppDbContext

---
+++ WizCoPedidos.WebApi/Data/AppDbContext.cs
@@ -0,0 +1,62 @@
+using Microsoft.EntityFrameworkCore;
+using WizCoPedidos.WebApi.Entidades;
+
+namespace WizCoPedidos.WebApi.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Pedido> Pedidos => Set<Pedido>();
+    public DbSet<ItemPedido> ItemsPedido => Set<ItemPedido>();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Pedido>(entity =>
+        {
+            entity.ToTable("Pedidos");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.ClienteNome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(x => x.DataCriacao)
+                .IsRequired();
+
+            entity.Property(x => x.Status)
+                .IsRequired();
+
+            entity.Property(x => x.ValorTotal)
+                .HasPrecision(18, 2);
+        });
+
+        modelBuilder.Entity<ItemPedido>(entity =>
+        {
+            entity.ToTable("ItensPedido");
+
+            entity.HasKey(x => x.Id);
+
+            entity.Property(x => x.ProdutoNome)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            entity.Property(x => x.Quantidade)
+                .HasPrecision(18, 2);
+
+            entity.Property(x => x.PrecoUnitario)
+                .HasPrecision(18, 2);
+
+            entity.HasOne<Pedido>()
+                .WithMany()
+                .HasForeignKey(x => x.PedidoId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
